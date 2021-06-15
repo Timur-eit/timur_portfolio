0194a5cf16a507a35ce2a714025ffde5d4fdc2a1
Evgeny <evtsyga@gmail.com>
2021-06-15 21:21:57 +0300

    prepare app architecture

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: 'react-app',
+    rules: {
+        // 'no-console': 'warn',
+        'semi': 'off',
+        'linebreak-style': ['warn', 'unix'],
+        "array-callback-return": "off"
+    },
+}
