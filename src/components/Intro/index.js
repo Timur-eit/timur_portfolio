6807fc0e51bc46351f5f4ffe2574c2e1f5a6e954
Evgeny <evtsyga@gmail.com>
2021-07-28 20:54:31 +0300

    update intro

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -5,15 +5,15 @@ function Intro() {
   return (
     <>
       <div className='intro-question' id='intro'>
-        <h1>Are you looking for a junior Frontend Developer?</h1>
+        <h1>Are you looking for a Frontend Developer?</h1>
       </div>
       <div className='intro'>
         <div className='intro__content'>
           <h1>Evgeny Tsygankov</h1>
-          <h2>junior frontend developer</h2>
+          <h2>Frontend Developer</h2>
           <img src={mainPicture} className='main-picture' alt='me'/>
           <p>
-            Hello, I focused on logistics for 10 years, then I met programming and fell in love to this activity, to the point,  that   I became  Junior Programmer. I write on JS, tried some Python, Haskell, Kotlin, C++. I sincerely love programming and I want  toprogress in    this area.
+            Hello, I focused on logistics for 10 years, then I met programming and fell in love to this activity, to the point,  that   I became a programmer. I write on JS, tried some Python, Haskell, Kotlin, C++. I sincerely love programming and I want  toprogress in    this area.
           </p>
         </div>
       </div>
