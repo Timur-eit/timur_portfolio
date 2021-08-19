60d436aa1db4886b661e8b70b5a75d92a3cda7f1
Evgeny <evtsyga@gmail.com>
2021-08-19 13:45:17 +0300

    clean up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import 'App.scss'
 import Intro from 'components/Intro'
 import Skills from 'components/Skills'
 import WorksList from 'components/WorksList'
 import Contacts from 'components/Contacts'
-// import BasicTextFields from 'shared/ui/Form'
 
 function App() {
   return (
