eb21b6ff3dcae273c551057c8feed152181ff410
Evgeny <evtsyga@gmail.com>
2022-05-02 20:07:06 +0300

    hide hh link

--- src/data/contactsData.ts
+++ src/data/contactsData.ts
@@ -1,7 +1,7 @@
 import emailImg from 'shared/img/contact_email.svg';
 import telegramImg from 'shared/img/contact_telegram.svg';
 import githubImg from 'shared/img/contact_github.svg';
-import headHunterImg from 'shared/img/contact_hh.png';
+// import headHunterImg from 'shared/img/contact_hh.png';
 import vkImg from 'shared/img/contact_vk.svg';
 
 export interface IMyContactList {
@@ -32,13 +32,13 @@ export const myContactList: IMyContactList = {
         linkPath: 'https://github.com/Timur-eit',
         altText: 'GitHub',
     },
-    headHunter: {
-        text: 'And my HeadHunter account',
-        imgSrc: headHunterImg,
-        linkPath:
-            'https://spb.hh.ru/resume/e9fad3c9ff08089fc00039ed1f6c3872725230',
-        altText: 'HeadHunter',
-    },
+    // headHunter: {
+    //     text: 'And my HeadHunter account',
+    //     imgSrc: headHunterImg,
+    //     linkPath:
+    //         'https://spb.hh.ru/resume/e9fad3c9ff08089fc00039ed1f6c3872725230',
+    //     altText: 'HeadHunter',
+    // },
     vk: {
         text: 'To know a bit more about me you can visit my VK page',
         imgSrc: vkImg,
